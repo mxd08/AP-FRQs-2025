ac8cc9a873ff0e586aed9a6cf619c3d8a88940fa
mxd08 <dmaksat15@gmail.com>
2025-05-13 11:31:56 -0400

    Update partA.java

--- Q2/partA.java
+++ Q2/partA.java
@@ -1,1 +1,25 @@
+public class SignedText{
+    private String first, last;
 
+    public SignedText(String a, String b){
+        first = a;
+        last = b;
+    }
+    public String getSignature(){
+        String result = "";
+        if(first.length()!=0){
+            result+=first.substring(0,1)+"-";
+        }
+        return result + last;
+    }
+    public String addSignature(String text){
+        String signature = getSignature();
+        if(text.indexOf(signature)==-1){
+            return text+signature;
+        } else if(text.indexOf(signature)==0){
+            return text.substring(signature.length())+signature;
+        } else {
+            return text;
+        }
+    }
+}
