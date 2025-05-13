d5e3c7196b51d466026b3c5a002db976587c28f9
mxd08 <dmaksat15@gmail.com>
2025-05-13 11:58:35 -0400

    Update partA.java

--- Q4/partA.java
+++ Q4/partA.java
@@ -1,1 +1,9 @@
-
+public SumOrSameGame(int numRows, int numCols){
+    puzzle[][] = new int[numRows][numCols];
+    for(int i = 0; i<numRows;i++){
+        for(int j = 0; j<numCols; j++){
+            int rand = (int)(Math.random()*9)+1;
+            puzzle[i][j]=rand;
+        }
+    }
+}
