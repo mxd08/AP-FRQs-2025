992cf60c7f49efb26a957fb4becfa904ddf62e53
mxd08 <dmaksat15@gmail.com>
2025-05-13 11:33:23 -0400

    Update partB.java

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,8 +1,4 @@
-# AP-FRQs-2025
-
-#Solutions written by Maksat Durdymyradov
-
- public int dogWalkShift(int startHour, int endHour){
+public int dogWalkShift(int startHour, int endHour){
     int moneyEarned = 0;
     for(int hour = startHour; hour<= endHour; hour++){
         int dogs = walkDogs(hour);
