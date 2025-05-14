5f07d411b24656968e6ef433f208c8cba97922c2
mxd08 <dmaksat15@gmail.com>
2025-05-14 11:32:30 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,9 @@
-
+public int walkDogs(int hour){
+    int companyDogs = numAvailableDogs(hour);
+    if(maxDogs>companyDogs){
+        updateDogs(hour,companyDogs);
+        return companyDogs;
+    }
+    updateDogs(hour,maxDogs);
+    return maxDogs;
+}
