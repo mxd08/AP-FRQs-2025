196a8be1685958d339335383df4427386ec9d883
mxd08 <dmaksat15@gmail.com>
2025-05-14 11:27:24 -0400

    Update partB.java

--- Q4/partB.java
+++ Q4/partB.java
@@ -1,1 +1,18 @@
-
+public boolean clearPair(int row, int col){
+    int num = puzzle[row][col];
+    for(int r = row; r<puzzle.length; r++){
+        for(int c = col; c<puzzle[0].length; c++){
+            if(num==puzzle[r][c]){
+                puzzle[row][col]=0;
+                puzzle[r][c]=0;
+                return true;
+            }
+            if(num+puzzle[row][c]==10){
+                puzzle[row][col]=0;
+                puzzle[row][c]=0;
+                return true;
+            }
+        }
+    }
+    return false;
+}
